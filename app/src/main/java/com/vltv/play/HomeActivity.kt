6ef83f071b1f414d207c1a1dd71bcb2c943c7ccc
leogracas34-gif <leogracas34@gmail.com>
2025-12-21 16:51:43 -0300

    HomeActivity.kt

--- app/src/main/java/com/vltv/play/HomeActivity.kt
+++ app/src/main/java/com/vltv/play/HomeActivity.kt
@@ -28,9 +28,6 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // REGISTRAR RECEIVER DE DOWNLOADS (ADICIONADO)
-        DownloadHelper.registerReceiver(this)
-
         setupClicks()
     }
 
