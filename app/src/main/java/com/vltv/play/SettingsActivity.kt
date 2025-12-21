57828c8f9ace60c864be0f80260aa466528d222e
leogracas34-gif <leogracas34@gmail.com>
2025-12-20 23:26:13 -0300

    SettingsActivity.kt

--- app/src/main/java/com/vltv/play/SettingsActivity.kt
+++ app/src/main/java/com/vltv/play/SettingsActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.vltv.play.BuildConfig
 
 class SettingsActivity : AppCompatActivity() {
 
